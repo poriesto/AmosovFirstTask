7a8bceeaece7ebdf825b162e52247a986402f2c4
poriesto <poriesto@hotmail.com>
2014-11-26 01:57:46 +0300

    some refactor

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include "math.hpp"
-using namespace std;
 
 int main(int argc, const char *argv[])
 {
-        vector<vector<int>> mat{
+        std::vector<std::vector<int>> mat{
 		{15,10,0,-6,17},
 		{3,14,8,9,-2},
 		{1,5,14,20,-3},
 		{7,19,10,2,0}
 	};
-	vector<int>X{100,150,200,250,300};
-	vector<double>P{0.1, 0.15, 0.25, 0.3, 0.2};
+	std::vector<int>X{100,150,200,250,300};
+	std::vector<double>P{0.1, 0.15, 0.25, 0.3, 0.2};
 	double param = 0.21;
   
-	cout << "Init matrix" << endl;
+	std::cout << "Init matrix" << std::endl;
 	print(mat);
 	MinMax(mat);
 	Sav(mat);
